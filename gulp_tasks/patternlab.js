bbb07c533f20991371ab3858f3714597f8f20029
Mark Miller <mrkmiller@ucdavis.edu>
2018-10-22 17:10:14 -0700

    update patternlab tasks to work with Node instead of PHP

--- gulp_tasks/patternlab.js
+++ gulp_tasks/patternlab.js
@@ -1,37 +1,50 @@
-const exec = require('child_process').exec;
 const browserSync = require('browser-sync');
+const fs = require('fs');
+const buffer = fs.readFileSync('./patternlab-config.json');
+const plConfig = JSON.parse(buffer.toString());
+const patternlab = require('@pattern-lab/core')(plConfig);
 
 module.exports = function (gulp, config, tasks) {
 
   // Compile Patternlab
-  gulp.task('patternlab', 'Compile Patternlab patterns into html.', function () {
-    // Setup a list of all commands to be run
-    const command = [
-      'php core/builder.php -gp',
-      'rsync -r --delete ' + config.patternLab.imagesSrc + '* ' + config.patternLab.imagesDest
-    ].join('&&');
-
-    // Execute the commands
-    exec(command, function (err, stdout, stderr) {
-      console.log(stdout);
-      console.log(stderr);
-
-      // Reload if using browsersync
-      if (config.browserSync.enabled) {
-        const server = browserSync.get('server');
-        if (server.active) {
-          server.reload();
-        }
-      }
-    });
+  gulp.task('patternlab', 'Build Patternlab patterns into html.', function (done) {
+    patternlab
+      .build({
+        cleanPublic: true,
+      }).then(() => {
+        done();
+      });
   });
   tasks.compile.push('patternlab');
 
+  gulp.task('patternlab:patterns', 'Compile Patternlab patterns into html.', function () {
+    patternlab
+      .patternsonly({
+        cleanPublic: plConfig.cleanPublic,
+      })
+      .then(() => {
+        if (config.browserSync.enabled) {
+          const server = browserSync.get('server');
+          if (server.active) {
+            server.reload();
+          }
+        }
+      });
+  });
+  tasks.compile.push('patternlab:patterns');
 
   // Watch for changes
   gulp.task('watch:markup', function () {
-    let tasks = ['patternlab'];
-    return gulp.watch(['source/_patterns/**/*.mustache', 'source/_patterns/**/*.json', 'source/_data/*.json'], tasks);
+    let tasks = ['patternlab:patterns'];
+
+    return gulp.watch([
+      'source/_patterns/**/*.hbs',
+      'source/_patterns/**/*.json',
+      'source/_data/*.json',
+      'source/_annotations/*.json',
+      'source/_meta/*.json',
+      'source/_pl/**/*'
+    ], tasks)
   });
   tasks.watch.push('watch:markup');
 
--- index.js
+++ index.js
@@ -18,10 +18,6 @@ module.exports = (gulpReference, userConfig, userTasks = {}) => {
   tasks = _.merge(tasks, userTasks);
 
   // Load in all tasks
-  if (config.patternLab.enabled) {
-    require('./gulp_tasks/patternlab.js')(gulp, config, tasks);
-  }
-
   if (config.themeSync.newsite) {
     require('./gulp_tasks/newsite.js')(gulp, config, tasks);
   }
@@ -30,16 +26,20 @@ module.exports = (gulpReference, userConfig, userTasks = {}) => {
     require('./gulp_tasks/theme-sync.js')(gulp, config, tasks);
   }
 
-  if (config.browserSync.enabled) {
-    require('./gulp_tasks/browser-sync.js')(gulp, config, tasks);
-  }
-
   if (config.js.enabled) {
     require('./gulp_tasks/js.js')(gulp, config, tasks);
   }
 
   if (config.css.enabled) {
     require('./gulp_tasks/css.js')(gulp, config, tasks);
+  }
+
+  if (config.patternLab.enabled) {
+    require('./gulp_tasks/patternlab.js')(gulp, config, tasks);
+  }
+
+  if (config.browserSync.enabled) {
+    require('./gulp_tasks/browser-sync.js')(gulp, config, tasks);
   }
 
   gulp.task('compile', 'Generate the entire site', tasks.compile);
