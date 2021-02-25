5bbf8aa68cb60a87d41f01279811f560bf5931ff
Mark Miller <mrkmiller@ucdavis.edu>
2021-02-24 16:58:12 -0800

    add a base snowpack config file

---
+++ snowpack.config.js
@@ -0,0 +1,19 @@
+// Base Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  exclude: [
+    '**/node_modules/**/*',
+    '**/sass/1_pattern_lab/**/*',
+    '**/README.md'
+  ],
+
+  plugins: [
+    '@snowpack/plugin-sass'
+  ],
+
+  devOptions: {
+    hmr: true
+  }
+};
