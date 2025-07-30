5411f9f5300456ba32257a24961fd95c341b271d
Mark Miller <mrkmiller@ucdavis.edu>
2025-07-29 20:24:36 -0700

    update start eslint config file

---
+++ starter/eslint.config.mjs
@@ -0,0 +1,16 @@
+export { default } from 'ucd-theme-tasks/eslint.config.default.mjs'
+
+/**
+ * Example of how to extend the default configuration.
+ */
+// import defaultConfig from "ucd-theme-tasks/eslint.config.default.mjs";
+// export default [
+//   ...defaultConfig,
+
+//   // My custom ESLint configuration.
+//   {
+//     rules: {
+//       "no-unused-vars": "warn",
+//     }
+//   }
+// ];
